1a53f94c65e2becc10bc9f86e7821a87f1f809bb
SebA-R <123127567+SebA-R@users.noreply.github.com>
2023-07-11 22:43:25 -0400

    Added parallax effect

--- src/components/pages/landing/index.js
+++ src/components/pages/landing/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import { useState, useRef, useEffect } from 'react';
 import videoBg from '../../../assets/banner_video.mp4';
+import './landing.css'
 
 //components
 import Header from './header-banner/index.js';
@@ -13,6 +14,7 @@ import Papers from './papers';
 import Teams from './teams/index.js';
 
 const Landing = () => {
+    
     //for handling the click states for the display of the menu
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
@@ -21,47 +23,62 @@ const Landing = () => {
     //for the handling of the max height
     const headerRef = useRef(null);
     const videoContentRef = useRef(null);
-    const [navMaxHeight, setNavMaxHeight] = useState(0);
-    const [videoMaxHeight, setVideoMaxHeight] = useState(0);
+    const navMaxHeight = useState(0);
+    const [scrollY, setScrollY] = useState(0);
+  
+
+    const handleScroll = () => {
+        setScrollY(window.scrollY);
+    };
+
     useEffect(() => {
-        const headerHeight = headerRef.current.clientHeight;
-        const videoContentHeight = videoContentRef.current.clientHeight;
-      
-        const calculatedNavMaxHeight = Math.min(10, headerHeight);
-        const calculatedVideoMaxHeight = Math.min(
-          videoContentHeight - calculatedNavMaxHeight,
-          videoContentHeight
-        );
-      
-        setNavMaxHeight(calculatedNavMaxHeight);
-        setVideoMaxHeight(calculatedVideoMaxHeight);
-      }, []);
-      
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const parallaxFactor = 0.5;
+    const parallaxOffset = scrollY * parallaxFactor;
 
     return (
         <>
-            <main id="home" className="antialiased w-full min-h-screen">
-                <Header navMaxHeight={navMaxHeight} headerRef={headerRef} />
-
-                <div id="video" className="relative -z-50 flex flex-col items-center justify-center">
-                    <video src={videoBg} autoPlay loop muted className="w-full h-full object-cover" />
-                    <div id="overlay" className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
-                    <div ref={videoContentRef} id="videoContent" className="absolute w-full h-full flex flex-col justify-center items-center text-neutral-100">
-                        <h1>Welcome</h1>
-                        <p>Scroll Down</p>
-                    </div>
-                </div>
-                <br/>
-                <Projects />
-                <div className="divider"></div>
-                <News />
-                <div className="divider"></div>
-                <Papers />
-                <div className="divider"></div>
-                <Teams />
-            </main>
+        <main id="home" className="antialiased w-full min-h-screen">
+        <Header navMaxHeight={navMaxHeight} headerRef={headerRef} />
+            <div
+            id="video"
+            className="relative -z-50 flex flex-col items-center justify-center"
+            style={{ transform: `translateY(${parallaxOffset}px)` }}
+            >
+            <video src={videoBg} autoPlay loop muted className="w-full h-full object-cover" />
+            <div id="overlay" className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+            <div ref={videoContentRef} id="videoContent" className="absolute w-full h-full flex flex-col justify-center items-center text-neutral-100">
+                <hgroup>
+                <h1>Acceleration Consortium</h1>
+                <p>---</p>
+                <h1>Robotics Assisted Accelerated Discovery</h1>
+                </hgroup>                
+                <svg className="arrows">
+                    <path className="a1" d="M0 0 L22.5 24 L45 0"></path>
+                    <path className="a2" d="M0 15 L22.5 39 L45 15"></path>
+                    <path className="a3" d="M0 30 L22.5 54 L45 30"></path>
+                </svg>
+            </div>
+            </div>
+            <br/>
+            <div className="content">
+            <Projects />
+            <div className="divider"></div>
+            <News />
+            <div className="divider"></div>
+            <Papers />
+            <div className="divider"></div>
+            <Teams />
+            </div>
+        </main>
         </>
     );
 };
 
 export default Landing;
+
