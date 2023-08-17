63ce63648a35cfa4379ddf5d7a18d29f6051a0f7
iamcolekim <76983801+iamcolekim@users.noreply.github.com>
2023-08-17 15:40:33 -0400

    small improvements and fixes

--- src/components/footer-banner/index.js
+++ src/components/footer-banner/index.js
@@ -1,21 +1,11 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-function NavButton({ linkTo, children }) {
-  const navigate = useNavigate();
-  return (
-    <>
-      <button className="text-green-500" onClick={() => navigate(linkTo)}>
-        {children}
-      </button>
-    </>
-  );
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 const Footer = () => {
-  return (
-    <footer>
-      <section>&copy; 2023 AC-RAAD | All Rights Reserved |</section>
-    </footer>
-  );
+    return (
+        <footer>
+            <section>&copy; 2023 AC-RAD | All Rights Reserved |</section>
+        </footer>
+    );
 };
 
 export default Footer;
